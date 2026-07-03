c4f932f6b6d0775f902cc15aee41b7ea53c7a0a0
Erick Daniel Peña Cedeño <erickdanielcedeno2123@gmail.com>
2026-07-02 20:00:49 -0500

    Agregar Dockerfile para Render

--- obj/Debug/net10.0/TaskFlow.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d79ac42f7674e6746a1004873b52e77ca3a6cba")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
